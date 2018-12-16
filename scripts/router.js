d8079608c72739b76ad4387fc632f14b12c4807e
MJWorkshop <mjworkshop95@gmail.com>
2018-12-16 12:25:38 +0800

    Add Router

---
+++ scripts/router.js
@@ -0,0 +1,41 @@
+app.config(function($routeProvider, $locationProvider){
+	'use strict';
+	$locationProvider.hashPrefix('');
+	$routeProvider
+	.when('/', {
+		templateUrl: 'dashboard.html'
+	})
+	.when('/dashboard', {
+		templateUrl: 'dashboard.html'
+	})
+    .when('/booking', {
+        templateUrl: function() {
+            return 'booking.html?' + new Date();
+        },
+        controller: 'bookingController'
+    })
+    .when('/booking/view/:id', {
+        templateUrl: function() {
+            return 'bookingView.html?' + new Date();
+        }
+    })
+    .when('/profile', {
+        templateUrl: function() {
+            return 'profile.html?' + new Date();
+        },
+        controller: 'profileController'
+    })
+    .when('/profile/edit/:id', {
+        templateUrl: function() {
+            return 'profileEdit.html?' + new Date();
+        }
+    })
+    .when('/profile/view/:id', {
+        templateUrl: function() {
+            return 'profileView.html?' + new Date();
+        }
+    })
+	.otherwise({
+		templateUrl: 'error-404.html'
+	});
+});
